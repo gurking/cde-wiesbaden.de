7c64df93029402d2bf60a4bf20d140e95fb40307
Florian Treder <flotreder@gmail.com>
2026-04-07 15:21:28 +0200

    Favicon added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxtjs/i18n'],
 
+  app: {
+    head: {
+      link: [
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ]
+    }
+  },
+
   devtools: {
     enabled: true
   },
